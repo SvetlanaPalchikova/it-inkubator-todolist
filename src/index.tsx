6edb761f4cfaaeb3b16e0d04e287ef78de0420ee
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2022-07-17 23:13:37 +0300

    lesson 11 useCallback, React.memo

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import AppWithRedux from "./state/AppWithRedux";
+import AppWithRedux from './AppWithRedux';
 import {store} from './state/store';
 import {Provider} from 'react-redux';
 
@@ -12,4 +11,7 @@ ReactDOM.render(
         <AppWithRedux/>
     </Provider>, document.getElementById('root'));
 
-serviceWorker.unregister()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
